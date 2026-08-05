202eb6e972e85b28b001c151a2788f8f1e906483
Tejas Kadu <tejaskadu9067@gmail.com>
2026-08-05 21:14:02 +0530

    Connect Express backend to MongoDB Atlas

---
+++ server/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log("====================================");
+    console.log("Mongo URI:");
+    console.log(process.env.MONGODB_URI);
+    console.log("====================================");
+
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed");
+    console.error(error);
+
+    process.exit(1);
+  }
+};
+
+export default connectDB;
