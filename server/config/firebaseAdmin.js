7a8a736589e99d2c10ffc770bcb78e182b81b542
Tejas Kadu <tejaskadu9067@gmail.com>
2026-08-08 20:39:46 +0530

    fix: use Firebase service account environment variable

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,18 +1,8 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, "../secrets/firebase-service-account.json"),
-    "utf8"
-  )
+  process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
 if (getApps().length === 0) {
